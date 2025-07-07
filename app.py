b9c6068e44cb7709c5ff8c2deef1df880901574b
Shivansh Jain <shivanshjain23@gmail.com>
2025-07-07 14:14:00 +0530

    app.py

--- app.py
+++ app.py
@@ -9,20 +9,18 @@ def predict(image):
     image = image.resize((224, 224))
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=0)
-
     prediction = model.predict(image)[0]
     class_names = ["Cat", "Dog", "Others"]
     pred_class = np.argmax(prediction)
     label = class_names[pred_class]
-
-    return f"<div style='text-align:center; font-size:24px; font-weight:bold'>{label}</div>"
+    return label
 
 interface = gr.Interface(
     fn=predict,
     inputs=gr.Image(type="pil"),
-    outputs=gr.HTML(),
-    title="Cat vs Dog Classifier",
-    description="Upload an image to predict whether it's a Cat, Dog, or something else (Others)"
+    outputs="label",
+    title="Cat vs Dog vs Classifier",
+    description="Upload an image to predict whether it's a Cat, Dog, or Others"
 )
 
 interface.launch()
